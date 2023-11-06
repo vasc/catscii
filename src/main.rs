f907a6b4398a0388df1523a0d5529e7086cec98e
Vasco Fernandes <git@vasco.ink>
2023-11-06 21:46:41 +0000

    Deserialize JSON response

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,24 @@
+use serde::Deserialize;
+
 #[tokio::main]
 async fn main() {
     let res = reqwest::get("https://api.thecatapi.com/v1/images/search")
         .await
         .unwrap();
-    println!("Status: {}", res.status());
-    let body = res.text().await.unwrap();
-    println!("Body: {}", body);
+    if !res.status().is_success() {
+        panic!("Request failed with HTTP {}", res.status());
+    }
+
+    #[derive(Deserialize)]
+    struct CatImage {
+        // this is the only field we really need, but let's show how we would
+        // deserialize the whole thing anyway.
+        url: String,
+    }
+
+    let images: Vec<CatImage> = res.json().await.unwrap();
+    let image = images
+        .first()
+        .expect("the cat API should return at least one image");
+    println!("The image is at {}", image.url);
 }
